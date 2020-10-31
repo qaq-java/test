282a22da6fbdc9671005a9e331d64af87d245a2c
chenchen <1021314672@qq.com>
2020-10-31 23:56:35 +0800

    '初始化项目'

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="goods">
+    <good-list-item v-for="item in goods" :goods-item="item"/>
+  </div>
+</template>
+
+<script>
+  import GoodListItem from "./GoodListItem";
+
+  export default {
+    name: "GoodsList",
+    components: {
+      GoodListItem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+
+
+
+</style>
---
+++ src/views/category/Category.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="wrapper" ref="aaa">
+    <ul>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+      <li>分类列表1</li>
+      <li>分类列表2</li>
+      <li>分类列表3</li>
+      <li>分类列表4</li>
+      <li>分类列表5</li>
+      <li>分类列表6</li>
+      <li>分类列表7</li>
+      <li>分类列表8</li>
+      <li>分类列表9</li>
+      <li>分类列表10</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Category",
+    data(){
+      return {
+        scroll: null
+      }
+    },
+    created() {
+      // console.log(this.$refs.aaa);
+      // console.log(document.querySelector('.wrapper'));
+      // this.scroll = new BScroll(document.querySelector('.wrapper'),{
+      // })
+    },
+    mounted() {
+      console.log(this.$refs.aaa);
+      console.log(document.querySelector('.wrapper'));
+
+      this.scroll = new BScroll(this.$refs.aaa)
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper{
+    height: 150px;
+    background-color: red;
+  }
+</style>
